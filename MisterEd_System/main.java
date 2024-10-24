dac4a92c71c38991c500004b871aca5e03ac4d23
Amir Abdullahi <abdullamir333@gmail.com>
2024-10-24 11:36:11 -0700

    aa

--- MisterEd_System/main.java
+++ MisterEd_System/main.java
@@ -1,15 +1,14 @@
-package MisterEd_System;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.Scanner;
 import java.time.format.DateTimeFormatter;
 
 
-import MisterEd_System.Patients.Patient;
+import MisterEd_System.Patient;
 public class Main {
     public static void main(String[] args) {
-        
-        
+        Patient patient = new Patient();
+        patient.getPromptInput();
         /*
         Amir - Prompt for patient - adds patient info and creates a patient class
         - the patient info gets passed off to triage
